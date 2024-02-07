bb67b1b290bd6c0f8ce9eaa65b372ddf999392e4
shamim.hossen <shamim.hossen@bjitgroup.com>
2024-02-07 16:31:29 +0600

    adding for loop tag

--- learn_django_templating/template_app/views.py
+++ learn_django_templating/template_app/views.py
@@ -6,5 +6,9 @@ def index(request):
     context = {
         'name': 'Shamim Hossen Razu',
         'age': 28,
-    }
+        'student': True,
+        'professional': False,
+        'subjects': ['Math', 'Physics', 'Chemistry', 'Biology', 'English'],
+        'marks': {'Math': 80, 'Physics': 85, 'Chemistry': 90, 'Biology': 95, 'English': 100},
+        }
     return render(request, 'index.html', context)
